7fc7c80a744b0727b1b8dbc5191e12f3e8126986
William <wsutanto@csu.fullerton.edu>
2023-03-23 00:56:30 -0700

    initial commit

---
+++ lab05-dakar4N15/mathematics/geometry/rectangle.py
@@ -0,0 +1,5 @@
+def perimeter(length, width):
+    return ((length*2) + (width*2))
+
+def area(length, width):
+    return(length*width)
---
+++ lab05-dakar4N15/mathematics/numbers/series.py
@@ -0,0 +1,11 @@
+def sum(list):
+    sum = 0
+    for a in list:
+        sum = a+sum
+    return sum
+
+def average(list):
+    sum = 0
+    for a in list:
+        sum = a+sum
+    return (sum/len(list))
