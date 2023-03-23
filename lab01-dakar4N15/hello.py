d4f456eadf3e9fbd19b77e431039cb37f79ba23c
William <wsutanto@csu.fullerton.edu>
2023-03-23 00:51:49 -0700

    initial commit

---
+++ lab01-dakar4N15/hello.py
@@ -0,0 +1,9 @@
+# Name: William Sutanto
+# Date: 1/26/2023
+# File Purpose: Multiple hello functions
+
+def helloworld():
+    return "Hello World!"
+
+def helloname(name):
+    return "Hello " + name + "!"
---
+++ lab01-dakar4N15/main.py
@@ -0,0 +1,11 @@
+# Name: William Sutanto
+# Date: 1/26/2023
+# File Purpose: Lab01 main program driver program
+
+from hello import *
+
+print("*** TUFFY TITAN HELLO PROGRAM ***")
+
+print(helloworld())
+
+print(helloname("William"))
